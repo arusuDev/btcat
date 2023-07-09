8e45bbc40c0fbed57690cb73d4832119efd9ea9c
arusuDev <arse.mria@gmail.com>
2023-07-09 23:37:31 +0900

    Add : MACD Test and change period

--- apps/ticker.go
+++ apps/ticker.go
@@ -19,12 +19,7 @@ func addPriceData(prices []float64, newPrice float64, period int) []float64 {
 // 価格を保持してテクニカル分析をする
 func CalcTechnical(prices, macdValues []float64, newPrice float64) (Technical, []float64, []float64) {
 	prices = addPriceData(prices, newPrice, conf.Period_tick)
-	// EMAs := EMA(prices, conf.TechnicalPeriod)
-	// if len(EMAs) >= 1 {
-	// 	fmt.Printf("EMA:%f", EMAs[len(EMAs)-1])
-	// 	return EMAs[len(EMAs)-1], prices
-	// }
-	// return prices[len(prices)-1], prices
+
 	macdValues, short, long, macd, signal, hist, err := MACD(macdValues, prices)
 	if err != nil {
 		log.Println(err)
--- conf/conf.go
+++ conf/conf.go
@@ -3,9 +3,9 @@ package conf
 const Period_tick = 200
 const TechnicalPeriod = 25
 
-const ShortEMAPeriod = 12
-const LongEMAPeriod = 26
-const SignalEMAPeriod = 9
+const ShortEMAPeriod = 35
+const LongEMAPeriod = 82
+const SignalEMAPeriod = 18
 
 // bitFlyerのリアルタイムAPIのエンドポイント
 const BitFlyerRealtimeEndpoint = "wss://ws.lightstream.bitflyer.com/json-rpc"
